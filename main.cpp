d87202dbd26e7aed338e7b0041202ed15e344204
Calvin <calvinloveland@gmail.com>
2018-02-05 22:49:08 -0700

    slight help changes

--- main.cpp
+++ main.cpp
@@ -6,25 +6,32 @@
 
 using namespace std;
 
+void printHelp() {
+	// The previous comment did not format well
+	cout << "--- Assign 2 Help ---\n";
+	cout << "-fib [n] : Compute the fibonacci of [n]\n";
+	cout << "-e [n] : Compute the value of 'e' using [n] iterations\n";
+	cout << "-pi [n] : Display Pi to [n] digits\n";
+}
+
 int main(int argc, char *argv[]) {
 	if (argc == 3) {
 		int n = stoi(argv[2]); // So stoi works I guess...
 		string argument = argv[1];
 		if (argument == "-fib") {
 			cout << "Fibonacci at " << n << " is " << fib(n) << "\n";
-		}
-		if (argument == "-pi") {
+		} else if (argument == "-pi") {
 			cout << "Pi until the " << n << "th digit is " << pi(n) << "\n";
-		}
-		if (argument == "-e") {
+		} else if (argument == "-e") {
 			cout.precision(10 + n);
 			cout << "E calculated with " << n << " iterations is " << e(n) << "\n";
+		} else {
+			printHelp();
+			return 1;
 		}
 	} else {
-		// The previous comment did not format well
-		cout << "--- Assign 2 Help ---\n";
-		cout << "-fib [n] : Compute the fibonacci of [n]\n";
-		cout << "-e [n] : Compute the value of 'e' using [n] iterations\n";
-		cout << "-pi [n] : Display Pi to [n] digits\n";
+		printHelp();
+		return 1;
 	}
+	return 0;
 }
